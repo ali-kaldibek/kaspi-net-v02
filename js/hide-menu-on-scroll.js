49fcc7b4c06b5088b242a3d49bc6b6fa29c66c44
Ali <131841612+ali-kaldibek@users.noreply.github.com>
2024-02-19 16:06:57 +0600

    Add files via upload

---
+++ js/hide-menu-on-scroll.js
@@ -0,0 +1,14 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0px";
+        // document.getElementsByTagName("header").style.top = "0";
+    } else {
+        // document.getElementById("navbar").style.top = "-50px";
+        document.getElementById("navbar").style.top = "-80px";
+
+        // document.getElementsByTagName("header").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+};
